edbb97239681ecf1937f8e2ccccc7a39e751cdc9
iliam9 <ilkafa93@yandex.ru>
2025-11-02 19:52:01 +0300

    Generate 16x16 grid of square divs using js

---
+++ script.js
@@ -0,0 +1,11 @@
+const container = document.getElementById('container');
+        const gridSize = 16;
+
+        function createGrid(size) {
+            for (let i = 0; i < size * size; i++) {
+                const square = document.createElement('div');
+                square.classList.add('grid-square');
+                container.appendChild(square);
+            }
+        }
+        createGrid(gridSize);
