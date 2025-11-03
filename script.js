0639f4e9a3809a0c2f3d2a231bb0b98477eb6dd8
iliam9 <ilkafa93@yandex.ru>
2025-11-03 20:16:28 +0300

    Remove hardcoded css square size. Implement dynamic square size in js. Add eventListener to change square backgroundColor

--- script.js
+++ script.js
@@ -4,9 +4,18 @@ const changeSizeBtn = document.getElementById('button');
         const gridSize = 16;
 
         function createGrid(size) {
+            //Полное обновление сетки. Удаляем ранее созданную сетку.
+            container.innerHTML = "";
+            const squareSize = 960 / size;
             for (let i = 0; i < size * size; i++) {
                 const square = document.createElement('div');
                 square.classList.add('grid-square');
+                square.style.width = `${squareSize}px`;
+                square.style.height = `${squareSize}px`;
+
+                square.addEventListener("mouseenter", 
+                    () => square.style.backgroundColor = 'black');
+
                 container.appendChild(square);
             }
         }
